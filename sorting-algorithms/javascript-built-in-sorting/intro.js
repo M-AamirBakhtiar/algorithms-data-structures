ddfd6291781c56aaf360be268254213066d4ff84
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2021-02-09 13:59:53 +0500

    added expected output for built-in sorting

--- sorting-algorithms/javascript-built-in-sorting/intro.js
+++ sorting-algorithms/javascript-built-in-sorting/intro.js
@@ -9,5 +9,5 @@ function decendingSort(a, b) {
 const nums = [12, 5, 63, 9, 47, 2, 1, 3, 14, 14];
 const nums2 = [12, 5, 63, 9, 47, 2, 1, 3, 14, 14];
 
-console.log(nums.sort(acendingSort));
-console.log(nums2.sort(decendingSort));
+console.log(nums.sort(acendingSort)); // Expected Output [ 1,  2,  3,  5,  9, 12, 14, 14, 47, 63 ]
+console.log(nums2.sort(decendingSort)); // Expected Output [ 63, 47, 14, 14, 12, 9,  5,  3,  2,  1 ]
